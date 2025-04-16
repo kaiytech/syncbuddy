e7a507b85920081c2d68caf657205666b0337361
Kajetan Krawczyk <kaitek666@gmail.com>
2025-04-16 14:44:54 +0200

    fixed gitignore which i accidentally moved

--- SyncBuddyCli/Program.cs
+++ SyncBuddyCli/Program.cs
@@ -1,14 +1,21 @@
 ﻿using CommandLine;
+using SyncBuddyLib;
 
 class Program
 {
     class Options
     {
-        [Option('n', "name", Required = true, HelpText = "Your name.")]
-        public string Name { get; set; }
+        [Option('s', "source", Required = true, HelpText = "Source directory")]
+        public string SourceDirectory { get; set; }
 
-        [Option('a', "age", Required = false, HelpText = "Your age.")]
-        public int Age { get; set; }
+        [Option('t', "target", Required = true, HelpText = "Target directory")]
+        public string TargetDirectory { get; set; }
+
+        [Option('i', "interval", HelpText = "Interval in minutes", Required = false)]
+        public int Interval { get; set; } = 0;
+
+        [Option('l', "log", Required = false, HelpText = "Log file path")]
+        public string Log { get; set; } = string.Empty;
     }
 
     static void Main(string[] args)
@@ -16,7 +23,55 @@ class Program
         Parser.Default.ParseArguments<Options>(args)
             .WithParsed(options =>
             {
-                Console.WriteLine($"Name: {options.Name}, Age: {options.Age}");
+                if (!Directory.Exists(options.SourceDirectory))
+                {
+                    Console.WriteLine($"Source directory '{options.SourceDirectory}' does not exist.");
+                    return;
+                }
+
+                if (!Directory.Exists(options.TargetDirectory))
+                {
+                    Console.WriteLine($"Target directory '{options.TargetDirectory}' does not exist.");
+                    return;
+                }
+
+                if (!SyncItem.AreDirectoriesIndependent(options.SourceDirectory, options.TargetDirectory))
+                {
+                    Console.WriteLine($"Provided directories are not independent.");
+                    return;
+                }
+
+                if (options.Interval < 0)
+                {
+                    Console.WriteLine($"Interval (when set) has to be a positive integer.");
+                    return;
+                }
+
+                if (options.Log != string.Empty)
+                {
+                    if (!Directory.Exists(Path.GetDirectoryName(options.Log)))
+                    {
+                        try
+                        {
+                            Directory.CreateDirectory(Path.GetDirectoryName(options.Log));
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine($"Failed to create log directory => {e.Message}");
+                            return;
+                        }
+                    }
+                    
+                    try
+                    {
+                        File.AppendAllText(options.Log, "");
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine($"Filed to write to log file => {e.Message}");
+                        return;
+                    }
+                }
             })
             .WithNotParsed(errors =>
             {
